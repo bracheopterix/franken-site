819c9f04bf1dc4c846e1964056fef26f28263eff
bracheopterix <bracheopterix@gmail.com>
2021-11-21 18:26:46 +0300

    saved origin

---
+++ jscode/themestoggle.js
@@ -0,0 +1,5 @@
+let.page = document.querySelector('.page');
+let.themeButton = document.querySelector('.themeButton');
+themeButton.onclick = function () {
+    page.classList.toggle('dark-theme')
+}
